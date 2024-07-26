43dcf77c3ead6a2bf049fe4c0edba32c333e17dd
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-07-26 21:26:42 +0300

    add konva with example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Konva from "konva"
+import { onMounted } from "vue"
+
+onMounted(() => {
+
+  const stage = new Konva.Stage({
+    container: 'container',
+    width: window.innerWidth,
+    height: window.innerHeight,
+  })
+
+  const layer = new Konva.Layer()
+  stage.add(layer)
+
+  const text1 = new Konva.Text({
+    x: 50,
+    y: 70,
+    fontSize: 30,
+    text: 'centeredScaling = true',
+    draggable: true,
+  })
+  layer.add(text1)
+
+  const tr1 = new Konva.Transformer({
+    nodes: [text1],
+    centeredScaling: true,
+  })
+  layer.add(tr1)
+
+  const text2 = new Konva.Text({
+    x: 50,
+    y: 200,
+    fontSize: 30,
+    text: 'centeredScaling = false',
+    draggable: true,
+  })
+  layer.add(text2)
+
+  const tr2 = new Konva.Transformer({
+    nodes: [text2],
+    centeredScaling: false,
+  })
+  layer.add(tr2)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="container">
+    i am some component in the ass
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style lang="scss">
+html, body {
+  margin: 0;
+  min-height: 100vh;
+  min-width: 100vw;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body, body * {
+  display: flex;
+  flex-direction: column;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</style>
+
+<style lang="scss" scoped>
+
 </style>
