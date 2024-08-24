af371686f0612ef3e4108d0807769cec21cbb355
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-08-24 20:47:12 +0300

    finalize header section for mobile

--- src/views/WelcomeHeader.vue
+++ src/views/WelcomeHeader.vue
@@ -3,6 +3,10 @@ import T2Logo from '@/assets/images/logo-black.svg'
 import { useI18n } from "vue-i18n"
 
 const { t } = useI18n()
+
+function handleReady() {
+
+}
 </script>
 
 <template>
@@ -11,11 +15,33 @@ const { t } = useI18n()
       <img :src="T2Logo" alt="logo">
       <div class="bar" />
     </div>
+    <div class="card">
+      <div class="content">
+        <h1>
+          {{ t('title.1') }}
+          <br>
+          <span style="color: var(--t2-pink)">{{ t('title.2') }}</span>
+        </h1>
+        <p>{{ t('body.1') }}</p>
+        <button class="t2 dark" @click="handleReady">{{ t('cta.ready') }}</button>
+      </div>
+      <img src="@/assets/images/illustrations/header-profile.png" alt="profile" class="profile">
+    </div>
+    <img src="@/assets/images/illustrations/sim.png" alt="sim" class="sim">
   </section>
 </template>
 
 <style scoped lang="scss">
 @import '@/assets/styles/mixins';
+
+#header {
+  position: relative;
+  gap: 5px;
+
+  @include large {
+    gap: 15px;
+  }
+}
 
 .toolbar {
   flex-direction: row;
@@ -49,13 +75,85 @@ const { t } = useI18n()
     border-radius: 30px;
   }
 }
+
+.card {
+  background-color: #EDEDEB;
+  position: relative;
+  overflow: hidden;
+
+  border-radius: 15px;
+  padding: 13px 13px 35vw 13px;
+
+  @include large {
+    border-radius: 30px;
+    padding: 50px;
+  }
+}
+
+.content {
+  background-color: black;
+  color: white;
+
+  border-radius: 10px;
+  padding: 10px;
+  gap: 15px;
+  font-size: 10px;
+
+  @include large {
+    font-size: 20px;
+    //max-width: 62.5%;
+  }
+
+  @include large {
+    border-radius: 20px;
+    padding: 30px 80px 50px 24px;
+    gap: 30px;
+  }
+
+  h1 {
+    font-family: "T2 Halvar Breit", sans-serif;
+    text-transform: uppercase;
+
+    font-size: 24px;
+
+    @include large {
+      font-size: 50px;
+    }
+  }
+
+  button {
+    margin-right: auto;
+
+    margin-top: 10px;
+
+    @include large {
+      margin-top: 20px;
+    }
+  }
+}
+
+.profile {
+  position: absolute;
+  bottom: -10vw;
+  right: -5vw;
+  pointer-events: none;
+  width: 65vw;
+}
+
+.sim {
+  position: absolute;
+  pointer-events: none;
+  width: 50vw;
+  left: 0;
+  bottom: 0;
+  transform: translateX(-20%);
+}
 </style>
 
 <i18n>
 ru:
   title.1: хотим разделить
   title.2: с вами важное
-  body.1: "Сегодня произошло крупное обновление: бренд Tele2 стал t2!\nКачество связи осталось, изменился только визуальный стиль"
-  body.2: Предлагаем сменить аватарку в телеграм на 1 день
+  body.1: "t2 — новый уровень Tele2. Мы изменили визуальный стиль, и предлагаем поддержать наше обновлении: опубликовать сторис и поставить стилизованную аватарку на один день."
   cta.ready: я готов!
 </i18n>
