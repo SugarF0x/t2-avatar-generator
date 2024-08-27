c2491fc74fc670a8a712740169b829703066d112
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-08-27 18:11:02 +0300

    smooth arrow

--- src/components/LineArrow.vue
+++ src/components/LineArrow.vue
@@ -3,14 +3,13 @@
 </script>
 
 <template>
-  <!-- TODO: decrease width and round edges -->
   <svg height="50" width="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" class="arrow">
     <defs>
-      <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="10" refY="3.5" orient="auto" markerUnits="strokeWidth">
-        <path d="M4,0 L10,3.5 L4,7" fill="none" stroke="currentColor" stroke-width="1" />
+      <marker id="arrowhead" markerWidth="10" markerHeight="8" refX="10" refY="4" orient="auto" markerUnits="strokeWidth">
+        <path d="M4,.5 L10,4 L4,7.5" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" />
       </marker>
     </defs>
-    <line x1="0" y1="25" x2="100%" y2="25" stroke="currentColor" stroke-width="5" marker-end="url(#arrowhead)" />
+    <line x1="2" y1="25" x2="99%" y2="25" stroke="currentColor" stroke-width="3" stroke-linecap="round" marker-end="url(#arrowhead)" />
   </svg>
 </template>
 
