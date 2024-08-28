d73ddd7638ec4645e199d1cc87aaff88fc5e14a6
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-08-28 12:26:42 +0300

    add image pre-processing

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VueI18nPlugin({
-      defaultSFCLang: 'yaml',
-    }),
+    VueI18nPlugin({ defaultSFCLang: 'yaml' }),
+    ViteImageOptimizer({})
   ],
   resolve: {
     alias: {
