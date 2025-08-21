402a9edd84ded13db1c1dc42ef874322502c6fa2
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-08-21 13:43:51 +0200

    update editor locales

--- src/views/AvatarEditor.vue
+++ src/views/AvatarEditor.vue
@@ -195,12 +195,12 @@ function handleDownload() {
 <i18n locale="ru">
 title: воспользуйтесь конструктором
 step.1.title: 'Шаг 1:'
-step.1.description: выберите портретное фото из галереи
+step.1.description: выберите фото
 step.2.title: 'Шаг 2:'
-step.2.description: выберите цвет фона, переключая кнопку
-instructions.1: Вы можете менять расположение фото, увеличивать или уменьшать так,
+step.2.description: выберите цвет
+instructions.1: "Вы можете менять расположение фото: поворачивать, увеличивать или уменьшать его так,"
 instructions.2: как вам нравится
-cta.photo: выбрать фото
+cta.photo: выбрать
 cta.download: мне нравится, скачать!
 cta.download.prompt: Ищите фото в загрузках своего браузера
 prompt.telegram: "Кажется, вы открыли эту страницу через встроенный браузер Telegram. Чтобы воспользоваться конструктором, пожалуйста, откройте страницу в другом браузере: нажмите на кнопку меню в верхней части экрана"
@@ -209,12 +209,12 @@ prompt.telegram: "Кажется, вы открыли эту страницу ч
 <i18n locale="en">
 title: use the editor
 step.1.title: 'Step 1:'
-step.1.description: select an image from your photo gallery
+step.1.description: select an image
 step.2.title: 'Step 2:'
-step.2.description: select a color by toggling the switch
+step.2.description: select a color
 instructions.1: You can adjust the position and scale of the image
 instructions.2: to your liking
-cta.photo: select image
+cta.photo: select
 cta.download: I like it, download!
 cta.download.prompt: Find the photo in your browser downloads directory
 prompt.telegram: "It seems that you’ve opened this page through Telegram’s built-in browser. To use the editor, please open the page in another browser: tap the menu button at the top of the screen."
